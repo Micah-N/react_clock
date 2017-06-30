0cce7bfc154a77e6bf5c0ac2def8cd3c4484e674
Micah-N <mnetz@2ndwatch.com>
2017-06-30 10:18:31 -0700

    Updating clock

--- src/clock.jsx
+++ src/clock.jsx
@@ -1,41 +1,13 @@
-function FormattedDate(props) {
-  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+function clock_tick() {
+  const element = (
+    <div>
+      <h3> Current (local) time is: {new Date().toLocaleTimeString()}.</h3>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('clock')
+  );
 }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <FormattedDate date={this.state.date} />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <Clock />,
-  document.getElementById('root')
-);
+setInterval(clock_tick, 1000);
